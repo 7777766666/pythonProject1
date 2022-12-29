a53fb3d945e1df8ca21224c0bb0b5aee18ed4739
777 <goszakupki.aktiv@gmail.com>
2022-12-29 20:19:19 +0300

    test2

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 print("Lol", 555, "999");
 print(print);
-name = input("Enter someThing: ");
-print(name )
+# name = input("Enter someThing: ");
+# print(name )
 lol = "Lol";
 print(dir(lol))
 x = 777;
 print(dir(x));
 print(lol.upper());
+
+print(dir())
+print(dir(__builtins__))
+input("Enter name, please: ");
+zzz = input("Enter name, please: ");
+print(zzz);

48142d8b6f9f3da8cede226b711f19eadc503574
777 <goszakupki.aktiv@gmail.com>
2023-01-01 18:24:29 +0300

    4

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ print(lol.upper());
 
 print(dir())
 print(dir(__builtins__))
+
+
 # input("Enter name, please: ");
 # zzz = input("Enter name, please: ");
 # ddd = input("age: ")
@@ -25,6 +27,7 @@ print(dir(__builtins__))
 
 def myFn():
     print("Hello");
+
 
 print("Lol");
 
@@ -86,18 +89,18 @@ print(name_lol[0]);
 print(name_lol[1]);
 print(name_lol[2]);
 print(name_lol[3]);
-print(name_lol[5 : 8]);
+print(name_lol[5: 8]);
 print(name_lol[:6]);
 print(name_lol[4:]);
 print(name_lol.count(name_lol));
 print(name_lol2.upper());
-print(name_lol2.count("bcdef"));  #содержит ли, аналог контэйн
+print(name_lol2.count("bcdef"));  # содержит ли, аналог контэйн
 name_new = name_lol2.replace("abc", "123");
 print(name_new);
-print(ppp.count("  "));  #количество данного выражения в нашей переменной 3*2  "  "
+print(ppp.count("  "));  # количество данного выражения в нашей переменной 3*2  "  "
 print(ppp[-2]);
 print(ppp[3: -2]);
-print(ppp[-2: ]);
+print(ppp[-2:]);
 
 x = "555";
 x = int(x);
@@ -106,7 +109,7 @@ x += 7.88888;
 print(x);
 print(type(x));
 
-print(pow(2, 10));  #2 в 10 степени 1024
+print(pow(2, 10));  # 2 в 10 степени 1024
 zz = 1_000_000;
 print(zz + x);
 
@@ -116,13 +119,13 @@ print(type(qqq));
 qqq = int(qqq);
 print(type(qqq));
 
-cc = 0.2+0.3+0.4+0.5+0.6+0.1+0.7
+cc = 0.2 + 0.3 + 0.4 + 0.5 + 0.6 + 0.1 + 0.7
 print(cc);
 print(type(cc));
 
 bb = "444.444";
 cc = float(bb);
-rr =  str(bb) + " hello " + str(type(bb));
+rr = str(bb) + " hello " + str(type(bb));
 print(rr);
 
 x = 66.66;
@@ -133,31 +136,31 @@ k = 2;
 complex_x = 10 + 9j;
 complex_y = 20 + 10j;
 
-print(str( complex_y + complex_x) + " and "+ str(type(complex_y)));
+print(str(complex_y + complex_x) + " and " + str(type(complex_y)));
 
-print ("lll" < "z")
+print("lll" < "z")
 
-print([1,2,3,9] > [1,9]);
+print([1, 2, 3, 9] > [1, 9]);
 print({a: 6} == {a: 6});
 
 ss = 2;
 ee = 2.5;
-print(ss+ee);
-print(ee+ss);
+print(ss + ee);
+print(ee + ss);
 
 nnn = True;
 bbb = 7;
 print(nnn + bbb);
-print(bbb+nnn);
+print(bbb + nnn);
 
 gg = "Lob ";
 g = 10;
 # print(g * gg);
 # print(gg * g);
 
-print(g.__mul__(gg));   #NotImplemented
-print(gg.__mul__(g));   #Lob Lob Lob Lob Lob Lob Lob Lob Lob Lob
-print(gg.__rmul__(g));  #Lob Lob Lob Lob Lob Lob Lob Lob Lob Lob
+print(g.__mul__(gg));  # NotImplemented
+print(gg.__mul__(g));  # Lob Lob Lob Lob Lob Lob Lob Lob Lob Lob
+print(gg.__rmul__(g));  # Lob Lob Lob Lob Lob Lob Lob Lob Lob Lob
 print(bool.__doc__);
 
 my_list = [];
@@ -171,7 +174,7 @@ number_list_1 = [100, 300, 1000];
 number_list_2 = [1000, 300, 100];
 user_input_list = ["Hello", 100, True, 99.99];
 
-print(number_list_1 == number_list_2);  #False
+print(number_list_1 == number_list_2);  # False
 print(number_list_1.__len__());
 print(len(number_list_2));
 print(len(number_list_2) == len(number_list_1));
@@ -193,8 +196,6 @@ users = [
         "name_user": "Tom"
     }
 ];
-
-
 
 print(len(users));
 print(users);
@@ -219,13 +220,12 @@ print(num_different);
 print(min(num_different));
 print(max(num_different));
 print(sum(num_different));
-print(sum(num_different)/len(num_different));
+print(sum(num_different) / len(num_different));
 num_different2 = [10, 10];
 num_different3 = num_different2 + num_different;
 print(num_different3);
 num_different4 = num_different2.__add__(num_different).__add__(num_different3);
 print(num_different4);
-
 
 # num_different.sort();
 print(num_different);
@@ -238,8 +238,8 @@ some_text = "Hello world for ever";
 some_text_lettes = list(some_text);
 print(some_text_lettes);
 
-users2 ={ "id_user": 888,
-          "name_user": "Ann" };
+users2 = {"id_user": 888,
+          "name_user": "Ann"};
 print(users2);
 users_letters = list(users2);
 print(users_letters);
@@ -268,7 +268,7 @@ num_different10.extend("abczzz");
 print(num_different10);
 
 list_l = [6, 8];
-f = {a : "Tom"};
+f = {a: "Tom"};
 
 list_rofl = [-6, True, "Lol", list_l, f];
 print(list_rofl);
@@ -279,7 +279,7 @@ print(list_rofl.__len__());
 list_rofl.reverse();
 print(list_rofl);
 list_rofl2 = ["mem", 99];
-list_rofl +=list_rofl2;
+list_rofl += list_rofl2;
 print(list_rofl);
 print(list_rofl[0:7]);
 
@@ -287,7 +287,7 @@ rofl = ["lol", "MegaLol"];
 rofl2 = [10, -2];
 rofl = rofl.__add__(rofl2);
 print(rofl);
-rofl +=rofl2;
+rofl += rofl2;
 print(rofl);
 
 my_car = {
@@ -324,7 +324,7 @@ print(my_car);
 lol_key = "model";
 my_car[lol_key] = "BMW";
 print(my_car);
-my_car["lol_inside" "price_in_uk"]= 65000;
+my_car["lol_inside" "price_in_uk"] = 65000;
 my_car["lol_inside""price_in_usa"] = 55_000;
 # my_car["lol_inside""different"] = True;
 print(my_car);
@@ -336,12 +336,12 @@ del my_car["lol_inside" "price_in_uk"];
 del my_car["lol_inside""price_in_usa"];
 print(len(my_car));
 print(my_car.get("some_key"));
-print(my_car.get("some_key",999));
+print(my_car.get("some_key", 999));
 
 lol_disk = {};
 print(id(lol_disk));
 print(type(lol_disk));
-lol_disk ["price"] = 100.5;
+lol_disk["price"] = 100.5;
 lol_disk["type"] = "ssd";
 print(lol_disk);
 print(lol_disk.__doc__);
@@ -424,7 +424,7 @@ print(lol_top);
 
 print(lol_top.index(999));
 zzzzz = lol_top.index(999);
-print(lol_top.index(999, zzzzz+1));
+print(lol_top.index(999, zzzzz + 1));
 print(lol_top);
 
 my_tuple = tuple("super lol");
@@ -435,7 +435,7 @@ print(my_tuple1);
 print(type(my_tuple1));
 my_tuple2 = (-7, False, "Lol");
 my_tuple3 = tuple("Hello, World");
-my_tuple4 = list(my_tuple2+my_tuple3);
+my_tuple4 = list(my_tuple2 + my_tuple3);
 print(my_tuple4);
 
 set_nums = {8, -88, 98, 8, 8};
@@ -443,7 +443,7 @@ set_dif = {99, True, "lol"};
 print(set_dif);
 print(type(set_dif));
 print(set_nums);
-set5 = {(55,55), 66, 66, 7, 66};
+set5 = {(55, 55), 66, 66, 7, 66};
 print(set5);
 set_str = {""};
 print(set_str);
@@ -457,7 +457,7 @@ photo_s = {"1900x2000", "555*666"};
 photo_x = {"2000*777", "555*666"};
 photo_x_s = photo_s.union(photo_x);
 photo_z = {"Super"};
-photo_x_s = photo_x_s|photo_z;
+photo_x_s = photo_x_s | photo_z;
 
 photo_x_s.add("Lol");
 print(photo_x_s);
@@ -477,7 +477,7 @@ print(jjj2);
 
 print(lol_set_lo1.intersection({7, 1, 9999999}));
 
-my_set4  = {"abc", "d", "dd","f", "y"};
+my_set4 = {"abc", "d", "dd", "f", "y"};
 print(my_set4.intersection("abcdef"));
 my_cort5 = ("a", "b", "c", "d", "y");
 print(my_set4.intersection(my_cort5));
@@ -510,7 +510,7 @@ print(type(list_convert));
 print(type(my_int7));
 
 my_range = range(10);
-my_range2= range(2, 17, 3);
+my_range2 = range(2, 17, 3);
 print(my_range);
 print(type(my_range));
 print(list(my_range));
@@ -552,7 +552,7 @@ cars = ["Tesla", "BMW", "Audi", "Kamaz"];
 price = [55, 30, 25, 111];
 in_office = (True, False, False, True)
 cars_price_zip1 = list(zip(cars, price, in_office));
-cars_price_zip2 = list(zip (in_office, price, cars));
+cars_price_zip2 = list(zip(in_office, price, cars));
 print(cars_price_zip2);
 print(type(cars_price_zip2));
 cars_price_zip5 = dict(zip(cars, price));
@@ -583,7 +583,7 @@ lol_not_clone = {
     "name": "Lol",
     "age": 1000.6,
 };
-print(str(id(lol_not_clone)) + " and \n" + str (id(about_lol)));
+print(str(id(lol_not_clone)) + " and \n" + str(id(about_lol)));
 copy_lol_clone = lol_not_clone.copy();
 
 lol_mem = {
@@ -596,7 +596,6 @@ print(lol_mem2["exchange"]);
 print(lol_mem["exchange"]);
 print(lol_mem2);
 print(lol_mem);
-
 
 lol_mem["exchange"].clear();
 print(lol_mem)
@@ -617,12 +616,15 @@ d = 101;
 def nyn(x, y):
     return x * y;
 
-print(nyn(a,b))
+
+print(nyn(a, b))
+
 
 def non(x, y):
     y += 100;
     c = x + y;
     print(c);
+
 
 non(a, b);
 print(type(non));
@@ -635,10 +637,13 @@ pers_lol = {
     "age": 30,
 }
 print(pers_lol["age"]);
+
+
 def increase_lol(xxx):
-    xxx["age"] +=100;
+    xxx["age"] += 100;
     print(id(xxx))
     return xxx;
+
 
 print(pers_lol["age"])
 
@@ -647,6 +652,7 @@ increase_lol(pers_lol)
 print(pers_lol);
 print(pers_lol["age"]);
 print(id(pers_lol))
+
 
 def method2(yyy):
     yyy_copy = yyy.copy();
@@ -660,35 +666,43 @@ print(pers_lol);
 print(pers_lol["age"]);
 print(lol_method2["age"]);
 
-r = [1,3]
+r = [1, 3]
 t = ["Lol", True, 987.0987]
+
+
 def merge_lists_to_dict(a, b):
     c = list(zip(a + b));
     return c;
+
 
 o = merge_lists_to_dict(r, t);
 v = merge_lists_to_dict(o, t);
 print(o);
 print(v);
 
-def mult(* x):
+
+def mult(*x):
     print(x);
     print(type(x));
     print(x[0]);
     print(x[1]);
     return sum(x);
 
+
 print(mult(3, 6, 11));
+
 
 def about_super_lol(car, model):
     any_lol = f"{car} is a {model} !!!";
     return any_lol;
+
 
 lols_car = about_super_lol("Tesla", 777);
 print(lols_car);
 
 lols_car2 = about_super_lol(model="BMW", car="My car");
 print(lols_car2);
+
 
 def get_car5(**car100):
     print(car100);
@@ -699,33 +713,40 @@ def get_car5(**car100):
     )
     return lols_car3;
 
-lols_car4 = get_car5( car1="Diesel", model1="Audi 8");
+
+lols_car4 = get_car5(car1="Diesel", model1="Audi 8");
 print(lols_car4);
+
 
 def merge_list_dict10(mem, fun):
     ppt = f" {mem} это очень {fun}";
     return ppt;
 
-rrr = merge_list_dict10(mem = "MEM", fun="смешно!");
+
+rrr = merge_list_dict10(mem="MEM", fun="смешно!");
 print(rrr);
 vvv = merge_list_dict10("Xa-Xa-Xa", fun="ga-ga-ga!");
 print(vvv);
 
+
 def update_car_info(**car9):
     print(type(car9));
     iii = (
-    f"{car9['brand']} is a baranNd and it cost "
-    f"{car9['price']} rubles!"
-    f"Is it good car = {car9['is_available']}"
+        f"{car9['brand']} is a baranNd and it cost "
+        f"{car9['price']} rubles!"
+        f"Is it good car = {car9['is_available']}"
     );
 
     return iii;
 
+
 ttt = update_car_info(brand="lada", price=12_000, is_available=True);
 print(ttt);
 
+
 def mult_test(num, mult=1):
     return num * mult;
+
 
 ee = mult_test(100);
 print(ee)
@@ -736,20 +757,35 @@ m = 66;
 eeee = mult_test(a, m);
 print(eeee);
 
+
 def get_today():
     return date.today().strftime('%A');
+
 
 def create_new_day_today(info, day=get_today()):
     info_copy = info.copy();
     info_copy["create_on"] = day;
     return info_copy;
 
+
 wright_any = {
     "num": 22,
     "make": True,
 }
-
 result_info = create_new_day_today(wright_any);
 print(result_info);
 
+def inside_lol(numb):
+    if(numb % 3) ==0:
+        print("You can / on 3");
+    else:
+        print("You cant / on 3....sorry")
 
+def outside_lol(numb, any_fn):
+    any_fn(user_num);
+
+
+user_num = int (input("Please, enter your number: "));
+print(user_num);
+
+outside_lol(user_num, inside_lol);
